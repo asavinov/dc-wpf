41b958c07bd84a9a2f9619986b863de6a2c747e4
Alexandr Savinov <savinov@conceptoriented.org>
2014-08-19 23:43:41 +0200

    Name change propagation through the schema

--- Samm/Dialogs/RenameBox.xaml.cs
+++ Samm/Dialogs/RenameBox.xaml.cs
@@ -29,9 +29,9 @@ namespace Samm.Dialogs
         public bool IsTable = false;
         public bool IsColumn = false;
 
-        protected CsSchema Schema { get { return (CsSchema)Element; } }
-        protected CsTable Table { get { return (CsTable)Element; } }
-        protected CsColumn Column { get { return (CsColumn)Element; } }
+        protected CsSchema Schema { get { if (Element is CsSchema) return (CsSchema)Element; else if (Element is CsTable) return ((CsTable)Element).Top; else if (Element is CsColumn) return ((CsColumn)Element).LesserSet.Top; else return null; } }
+        protected CsTable Table { get { if (Element is CsSchema) return (CsTable)Element; else if (Element is CsTable) return ((CsTable)Element); else if (Element is CsColumn) return ((CsColumn)Element).LesserSet; else return null; } }
+        protected CsColumn Column { get { if (Element is CsSchema) return null; else if (Element is CsTable) return null; else if (Element is CsColumn) return (CsColumn)Element; else return null; } }
 
 
         public RenameBox(object element, string name)
@@ -72,14 +72,17 @@ namespace Samm.Dialogs
             // Really rename the element and propagate this change through all objects where this element name is used
             if (Element is CsSchema)
             {
+                Schema.RenameTable(Schema, NewName);
                 Schema.Name = NewName;
             }
             else if (Element is CsTable)
             {
+                Schema.RenameTable(Table, NewName);
                 Table.Name = NewName;
             }
             else if (Element is CsColumn)
             {
+                Schema.RenameColumn(Column, NewName);
                 Column.Name = NewName;
             }
 
